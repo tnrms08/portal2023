99e6bfab50a832e3f2a4eaf7ec4edbc3dd3e11db
tnrms08 <wjddlfla661@naver.com>
2023-03-31 18:03:35 +0900

    5-4. UserDao에서의 장애 해결

--- userdao/src/main/java/kr/ac/jejunu/userdao/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/userdao/user/UserDao.java
@@ -11,43 +11,76 @@ public class UserDao {
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();    //Method로 리팩토링(Ctrl+Alt+M)
-        //쿼리 만들고
-        PreparedStatement preparedStatement = connection.prepareStatement
-                ("select id, name, password from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        //쿼리 실행하고
-        ResultSet resultSet = preparedStatement.executeQuery();
-        resultSet.next();
-        //결과를 사용자 정보에 매핑하고
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-        //결과리턴
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null; //오류 발생
+        User user;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = connection.prepareStatement
+                    ("select id, name, password from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+       
         return user;
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
-        Connection connection = dataSource.getConnection();
-        //쿼리 만들고
-        PreparedStatement preparedStatement = connection.prepareStatement
-                ("insert into userinfo (name, password) values ( ?, ? )"
-                        , Statement.RETURN_GENERATED_KEYS);
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
-        //쿼리 실행하고
-        preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        user.setId(resultSet.getLong(1));
-        //자원해지
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            //쿼리 만들고
+            preparedStatement = connection.prepareStatement
+                    ("insert into userinfo (name, password) values ( ?, ? )"
+                            , Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
+            //쿼리 실행하고
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getLong(1));
+        } finally {
+            //자원해지
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
