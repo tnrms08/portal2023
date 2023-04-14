941c259c3c1b8fc8156f9e6cd3db8db674d2c029
tnrms08 <wjddlfla661@naver.com>
2023-04-14 16:55:16 +0900

    리팩토링 완결..??

--- userdao/src/main/java/kr/ac/jejunu/userdao/user/JdbcContext.java
+++ userdao/src/main/java/kr/ac/jejunu/userdao/user/JdbcContext.java
@@ -1,11 +1,9 @@
 package kr.ac.jejunu.userdao.user;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
+// 변하지 않는 내용들을 넣어 놓은 것
 public class JdbcContext {
     private final DataSource dataSource;
 
@@ -101,5 +99,38 @@ public class JdbcContext {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    void update(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for(int i = 0; i< params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        };
+        jdbcContextForUpdate(statementStrategy);
+    }
+
+    void insert(User user, String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i = 0; i< params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        };
+        jdbcContextForInsert(user, statementStrategy);
+    }
+
+    User find(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for(int i = 0; i< params.length; i++){
+                preparedStatement.setObject(i+1, params[i]);
+            }
+            return preparedStatement;
+        };
+        return jdbcContextForFind(statementStrategy);
     }
 }
