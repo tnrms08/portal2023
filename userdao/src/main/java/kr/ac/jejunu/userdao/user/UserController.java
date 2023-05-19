94bfa9a71f6b46464fa05e60e0fe4df1eb0ea0c9
tnrms08 <wjddlfla661@naver.com>
2023-05-19 17:20:09 +0900

    content negotiation view resolver

--- userdao/src/main/java/kr/ac/jejunu/userdao/user/UserController.java
+++ userdao/src/main/java/kr/ac/jejunu/userdao/user/UserController.java
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping(value = "/user")
+@RequestMapping
 @RequiredArgsConstructor
 public class UserController {
     private final UserDao userDao;
 
-    @RequestMapping
+    @RequestMapping(path = "/user", produces = "text/html")
     public User getUser(@RequestParam Long id){
         return userDao.findById(id);
     }
